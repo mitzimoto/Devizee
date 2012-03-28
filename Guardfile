8e6571ab015497511b33bcf9309ac38e6b402f31
eric <eric@eric-Latitude-D630.(none)>
2012-03-27 20:35:36 -0400

    initial commit

---
+++ Guardfile
@@ -0,0 +1,6 @@
+# A sample Guardfile
+# More info at https://github.com/guard/guard#readme
+
+guard 'less', :all_on_start => true, :all_after_change => true, :output => 'vendor/assets/stylesheets' do
+  watch(%r{^.+\.less$})
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Realtorest::Application.config.secret_token = '9f00c7bc5cee35b49e71f190b6b878fa46cc5e9fa59b27a0a9c687db4372480f4cb77fa457116e66bbce8bcb5cfe9cc816c8befac23c341ed36968a59302b898'
